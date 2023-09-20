09a54ef6bf2e32756c2cbb2497276da079d81fa9
rahul <rahulgoswami1520@gmail.com>
2023-09-20 21:01:18 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./style.css"
+import images from "./Images";
+import { useState } from "react";
+import {  shuffle } from "lodash";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [cards, setCards] = useState(shuffle([...images, ...images]));
+    const [activeCards,setActiveCard]=useState([]);
+    const [gameOver,setGameOver]=useState(false)
+    const [foundpairs,setfoundPairs]=useState([])
+    const [clicks,setClicks]=useState(0)
+    function flip(index) {
+        if(gameOver)
+        {
+            setCards(shuffle([...images,...images]))
+            setfoundPairs([])
+            setGameOver(false)
+            setClicks(0)
+        }
+        if(activeCards.length===0)
+        {
+            setActiveCard([index])
+        }
+        if(activeCards.length===1)
+        {
+            let firstIndex=activeCards[0]
+            let secondIndex=index;
+            if(cards[firstIndex]===cards[secondIndex])
+            {
+                setfoundPairs([...foundpairs,firstIndex,secondIndex])
+            }
+            if(foundpairs.length + 2 === cards.length)
+            {
+                setGameOver(true)
+            }
+            setActiveCard([...activeCards,index])
+        }
+        if(activeCards.length===2)
+        {
+            
+            setActiveCard([index])
+        }
+        setClicks(clicks+1)
+    }
+    return (
+        <div>
+            <div className="data">
+                {
+                    gameOver&&<p>you won the game!</p>
+                }
+                Clicks: {clicks}, found pairs: {(foundpairs.length)/2}
+            </div>
+            <div className="board">
+                {
+                    cards.map((card, index) => {
+                        const flippedTofront=(activeCards.indexOf(index)!==-1) || foundpairs.indexOf(index)!==-1;
+                    return (
+                        <div className={"card-outer " +(flippedTofront?"flipped":"")} key={index} onClick={()=>flip(index)}>
+                                <div className="card">
+                                    <div className="front">
+                                        <img src={card} alt="meme" />
+                                    </div>
+                                    <div className="back">
+                                        <div></div>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+
+
+            </div>
+            
+            
+        </div>
+    )
 }
 
 export default App;
---
+++ src/Images.js
@@ -0,0 +1,23 @@
+import image1 from "./memes/01.jpg";
+import image2 from "./memes/02.jpg";
+import image3 from "./memes/03.jpg";
+import image4 from "./memes/04.jpg";
+import image5 from "./memes/05.jpg";
+import image6 from "./memes/06.jpg";
+import image7 from "./memes/07.jpg";
+import image8 from "./memes/08.jpg";
+import image9 from "./memes/09.jpg";
+import image10 from "./memes/10.jpg";
+import image11 from "./memes/11.jpg";
+import image12 from "./memes/12.jpg";
+import image13 from "./memes/13.jpg";
+import image14 from "./memes/14.jpg";
+import image15 from "./memes/15.jpg";
+import image16 from "./memes/16.jpg";
+import image17 from "./memes/17.jpg";
+import image18 from "./memes/18.jpg";
+
+
+const images = [image1,image2,image3,image4,image5,image6,image7,image8,image9,image10,image11,image12,image13,image14,image15,image16,image17,image18];
+
+export default images;
